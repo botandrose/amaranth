1c4068f6293e03a66a02c0001c3aeaebeb2c8f95
Micah Geisel <micah@botandrose.com>
2015-12-17 11:05:25 -0800

    enable focus testing.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,9 @@ require 'amaranth'
 require 'byebug'
 
 RSpec.configure do |config|
+  config.filter_run :focus
+  config.run_all_when_everything_filtered = true
+
   config.mock_with(:rspec) { |c| c.syntax = :should }
   config.expect_with(:rspec) { |c| c.syntax = :should }
 end
